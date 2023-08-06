9440cf921968701ae8310dc3f9d0116791366108
Eduardo <eduardoalves3456@hotmail.com>
2023-08-05 22:48:32 -0300

    include method update and delete

--- src/main/java/com/gamelibrary/controller/GameController.java
+++ src/main/java/com/gamelibrary/controller/GameController.java
@@ -2,6 +2,7 @@ package com.gamelibrary.controller;
 
 import com.gamelibrary.model.GameModel;
 import com.gamelibrary.repository.GameRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,20 +12,21 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/game")
 public class GameController {
 
     @Autowired
     GameRepository gameRepository;
 
-    @PostMapping("/game")
+    @PostMapping()
     public ResponseEntity<GameModel> saveGame(@RequestBody GameModel gameModel ) {
         return ResponseEntity.status(HttpStatus.CREATED).body(gameRepository.save(gameModel));
     }
-    @GetMapping("/game")
+    @GetMapping()
     public ResponseEntity<List<GameModel>> getAllGame() {
         return ResponseEntity.status(HttpStatus.OK).body(gameRepository.findAll());
     }
-    @GetMapping("/game/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Object> getOneGame(@PathVariable (value = "id") Long id) {
         Optional<GameModel> gameO = gameRepository.findById(id);
         if(gameO.isEmpty()) {
@@ -32,7 +34,24 @@ public class GameController {
         }
         return ResponseEntity.status(HttpStatus.OK).body(gameO.get());
     }
-    
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateGame (@PathVariable (value = "id") Long id,@RequestBody GameModel gameModel) {
+        Optional<GameModel> game0 = gameRepository.findById(id);
+        if(game0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Game not found. ");
+        }
+        BeanUtils.copyProperties(gameModel,game0.get());
+        return ResponseEntity.status(HttpStatus.OK).body(gameRepository.save(game0.get()));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteOneGame (@PathVariable (value = "id") Long id) {
+        Optional<GameModel> game0 = gameRepository.findById((id));
+        if(game0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Game not found. ");
+        }
+        gameRepository.delete(game0.get());
+        return ResponseEntity.status(HttpStatus.CREATED).body("deleted successfully. ");
+    }
 
 
 }
